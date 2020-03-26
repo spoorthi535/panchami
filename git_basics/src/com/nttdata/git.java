7f611a6c65893b7d72c97f422a5f939dbf87552b
Spoorthi Ambarish <spoorthi162@gmail.com>
2020-03-26 14:40:59 +0530

    bye

---
+++ git_basics/src/com/nttdata/git.java
@@ -0,0 +1,10 @@
+package com.nttdata;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi welcome to git basics");
+	}
+
+}
